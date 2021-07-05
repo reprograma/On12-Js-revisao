7aefabb511b86818306a6c225630d8ca1c63bdf9
Mayhhara Morais <mayhhara.morais@wilcompute.com>
2021-07-05 10:55:10 -0300

    resolucao atividade array

---
+++ arrays-loops/atividade07/index.js
@@ -0,0 +1,9 @@
+//Tente fazer sozinha, resposta para conferência.
+const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomesA = [];
+
+for(let nome of nomes){
+    if(nome[0] === "A" || nome[0] === "a"){
+        nomesA.push(nome);
+    }
+}console.log(nomesA);
